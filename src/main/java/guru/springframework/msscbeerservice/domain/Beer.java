007941a4002acd1250eed60536e3c907fc04e220
Andersson Garcia Sotelo <andegaso@suramericana.com.co>
2023-05-30 10:37:34 -0500

    Added min changes

--- src/main/java/guru/springframework/msscbeerservice/domain/Beer.java
+++ src/main/java/guru/springframework/msscbeerservice/domain/Beer.java
@@ -1,15 +1,25 @@
 package guru.springframework.msscbeerservice.domain;
 
-import lombok.*;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Version;
+
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Type;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.*;
-import java.math.BigDecimal;
-import java.sql.Timestamp;
-import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * Created by jt on 2019-05-17.
--- src/main/java/guru/springframework/msscbeerservice/web/controller/MvcExceptionHandler.java
+++ src/main/java/guru/springframework/msscbeerservice/web/controller/MvcExceptionHandler.java
@@ -1,13 +1,14 @@
 package guru.springframework.msscbeerservice.web.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintViolationException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import javax.validation.ConstraintViolationException;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by jt on 2019-05-25.
@@ -15,13 +16,13 @@ import java.util.List;
 @ControllerAdvice
 public class MvcExceptionHandler {
 
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<List> validationErrorHandler(ConstraintViolationException ex){
-        List<String> errorsList = new ArrayList<>(ex.getConstraintViolations().size());
+	@ExceptionHandler(ConstraintViolationException.class)
+	public ResponseEntity<List<String>> validationErrorHandler(ConstraintViolationException ex) {
+		List<String> errorsList = new ArrayList<>(ex.getConstraintViolations().size());
 
-        ex.getConstraintViolations().forEach(error -> errorsList.add(error.toString()));
+		ex.getConstraintViolations().forEach(error -> errorsList.add(error.toString()));
 
-        return new ResponseEntity<>(errorsList, HttpStatus.BAD_REQUEST);
-    }
+		return new ResponseEntity<>(errorsList, HttpStatus.BAD_REQUEST);
+	}
 
 }
